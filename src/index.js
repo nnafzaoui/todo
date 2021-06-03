477a14d9026c77e575e9c42048a84785d48557f8
nnafzaoui <nafzaouinourdine@gmail.com>
2021-06-03 14:15:56 +0100

    ey

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
-
 const api = express();
 
 
-api.get('/', (req, res) => {
-    console.log(req);
-    res.send('Hello, world!');
-  });
+// api.get('/', (req, res) => {
+//     console.log(req);
+//     res.send('Hello, world!');
+// });
+api.use((req, res, next) => {
+    console.log('Hello');
+    next();
+});
+
+api.use(express.static(__dirname + '/public'));
 
 api.listen(3000, () => {
   console.log('API up and running!');
