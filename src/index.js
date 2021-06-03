d83e1b4877c57b24def2f7a280fdcd8f119dc956
nnafzaoui <nafzaouinourdine@gmail.com>
2021-06-03 15:03:18 +0100

    logged items

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const api = express();
+const bodyParser = require('body-parser');
 
 
 // api.get('/', (req, res) => {
 //     console.log(req);
 //     res.send('Hello, world!');
 // });
+
+api.use(bodyParser.json());
+
+
 api.use((req, res, next) => {
     console.log('Hello');
     next();
 });
 
 api.use(express.static(__dirname + '/public'));
+
+api.post('/add', (req, res) => {
+  
+  console.log(req.body);
+  res.send('It works!');
+
+ });
 
 api.listen(3000, () => {
   console.log('API up and running!');
